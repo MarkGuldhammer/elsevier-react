38650d87cf541fdfbf93695a87b1b10cbdf77240
Mark Guldhammer <markguldhammer@gmail.com>
2019-11-24 13:25:53 +0100

    removed bloat, setup basic structure

---
+++ src/employees/EmployeeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world employee list</h1>
+      </div>
+    );
+  }
+}
+
+export default EmployeeList
