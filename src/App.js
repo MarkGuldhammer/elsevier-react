c5d3616c53e2beaa7812aafcb65d8af3f515ba28
Mark Guldhammer <markguldhammer@gmail.com>
2019-11-24 18:30:34 +0100

    added sorting, lesser files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
-import EmployeeList from './employees/EmployeeList';
+import React, {Component} from 'react';
+import EmployeeTable from './employees/EmployeeTable';
+import EmployeeData from './json/generated.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: EmployeeData
+    }
+    
+    this.sortBy = this.sortBy.bind(this)
+  }
+
+  sortBy(key) {
+    this.setState({
+      data: EmployeeData.sort( (a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0))
+    })
+  }
+  
   render() {
     return (
       <div>
-        <EmployeeList />
+        <EmployeeTable 
+          data={this.state.data}
+          sortBy={this.sortBy}
+        />
       </div>
     )
   }
---
+++ src/employees/EmployeeTable.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function EmployeeTable(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <button onClick={() => props.sortBy('id')}>Id</button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy('name')}>Name</button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy('gender')}>Gender</button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy('company')}>Company</button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy('email')}>Email</button>
+          </th>
+          <th>Is Active</th>
+          <th>Picture</th>
+        </tr>
+        </thead>
+          <tbody>
+            {
+              props.data.map((row, index) => (
+                <tr key={index}>
+                  <td>{row.id}</td>
+                  <td>{row.name}</td>
+                  <td>{row.gender}</td>
+                  <td>{row.company}</td>
+                  <td>{row.email}</td>
+                  <td>{row.isActive ? 'true' : 'false'}</td>
+                  <td><img src={row.picture} alt=''/></td>
+                </tr>
+              ))
+            }
+          </tbody>
+      </table>
+    )
+}
